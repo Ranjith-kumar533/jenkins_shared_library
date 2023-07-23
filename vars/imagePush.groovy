8f7164620839542407b4534eb2b4f2a80e0d37df
Kongeswaran <ranjithkumar.kongeswaran@igt.com>
2023-07-23 19:17:50 +0530

    test

--- vars/imagePush.groovy
+++ vars/imagePush.groovy
@@ -1,5 +1,5 @@
 def call(String uname, String repo, String tag ){
-    def image = ${uname}/${repo}:${tag} 
+    def image = "${uname}/${repo}:${tag}"
     // This step should not normally be used in your script. Consult the inline help for details.
     withDockerRegistry(credentialsId: 'Docker_creds', url: 'https://index.docker.io/v1/') {
     image.push()
