440c258eea61112a2e4856950e75df0fdd471bb5
Kongeswaran <ranjithkumar.kongeswaran@igt.com>
2023-07-24 19:04:48 +0530

    new changes

--- vars/imageCleanup.groovy
+++ vars/imageCleanup.groovy
@@ -1,3 +1,9 @@
-def call(String uname, String repo, String tag ){
-    sh 'docker rmi ${uname}/${repo}:${tag}'
+def call(String uname, String repo, String tag, String repository, String region ){
+   
+     if (repository = "Docker"){
+     sh "docker rmi ${uname}/${repo}:${tag}"
+    }
+    else{
+    sh "docker rmi ${uname}.dkr.ecr.${region}.amazonaws.com/${repo}:${tag}"
+    }
 }
--- vars/imageScan.groovy
+++ vars/imageScan.groovy
@@ -1,3 +1,8 @@
-def call(String uname, String repo, String tag ){
+def call(String uname, String repo, String tag, String repository, String region){
+    if (repository = "Docker"){
     sh "trivy image ${uname}/${repo}:${tag} > imagescanreport.txt"
+    }
+    else{
+    sh "trivy image ${uname}.dkr.ecr.${region}.amazonaws.com/${repo}:${tag} > imagescanreport.txt"
+    }
 }
